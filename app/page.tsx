67077b93eff722300ee1dbf279e9500ef6d7d75b
TIDJANI BACHIR <chiro.tidjani@gmail.com>
2024-09-08 21:32:51 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,14 @@ import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
+
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import {DisktopApp} from "@/components/DisktopApp";
-import {PresentMobile} from "@/components/PresentMobile";
-import UisShowCases from "@/components/UisShowCases";
+
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import {PresentMain} from "@/components/PresentMain";
-import {MobileAppsShowCase} from "@/components/MobileAppsShowCase";
-import {MobileAppsShowCase2} from "@/components/MobileAppsShowCase2";
-import {MainFrontendApp} from "@/components/MainFrontendApp";
-import ChangeLanguage from "@/components/ChangeLanguage";
+
 
 
 // import {PresentBestProject} from "@/components/PresentBestProject";
